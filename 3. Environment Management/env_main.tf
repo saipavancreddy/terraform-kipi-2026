c3ccbe9d19e006ceb675160b569d4a1faefe8788
saipavancreddy <saipavan.c.reddy@kipi.ai>
2026-04-21 11:14:27 +0530

    Update env_main.tf

--- 3. Environment Management/env_main.tf
+++ 3. Environment Management/env_main.tf
@@ -1,7 +1,3 @@
-############################################
-# WAREHOUSE
-############################################
-
 resource "snowflake_warehouse" "env_wh" {
   name           = var.warehouse_name
   warehouse_size = var.warehouse_size
@@ -10,34 +6,18 @@ resource "snowflake_warehouse" "env_wh" {
   initially_suspended = true
 }
 
-############################################
-# DATABASE
-############################################
-
 resource "snowflake_database" "env_db" {
   name = var.db_name
 }
-
-############################################
-# SCHEMA
-############################################
 
 resource "snowflake_schema" "raw_schema" {
   database = snowflake_database.env_db.name
   name     = "RAW"
 }
 
-############################################
-# ROLE
-############################################
-
 resource "snowflake_account_role" "env_role" {
   name = var.access_role
 }
-
-############################################
-# GRANT WAREHOUSE ACCESS
-############################################
 
 resource "snowflake_grant_privileges_to_account_role" "warehouse_usage" {
   privileges        = ["USAGE"]
@@ -49,10 +29,6 @@ resource "snowflake_grant_privileges_to_account_role" "warehouse_usage" {
   }
 }
 
-############################################
-# GRANT DATABASE ACCESS
-############################################
-
 resource "snowflake_grant_privileges_to_account_role" "db_usage" {
   privileges        = ["USAGE"]
   account_role_name = snowflake_account_role.env_role.name
@@ -62,10 +38,6 @@ resource "snowflake_grant_privileges_to_account_role" "db_usage" {
     object_name = snowflake_database.env_db.name
   }
 }
-
-############################################
-# GRANT SCHEMA ACCESS
-############################################
 
 resource "snowflake_grant_privileges_to_account_role" "schema_usage" {
   privileges        = ["USAGE"]
