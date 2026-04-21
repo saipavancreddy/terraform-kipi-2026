08e7b889f2cab39684feef2d6fa0c8f4152fb71a
saipavancreddy <saipavan.c.reddy@kipi.ai>
2026-04-21 11:13:25 +0530

    Update roles.tf

--- 2. Role-Based Access Control (RBAC) /roles.tf
+++ 2. Role-Based Access Control (RBAC) /roles.tf
@@ -1,69 +1,43 @@
-############################################
-# ROLES
-############################################
-
-# Custom SYSADMIN Role
 resource "snowflake_account_role" "sysadmin_custom" {
   name = "SYSADMIN_CUSTOM"
 }
 
-# Data Engineer Role
 resource "snowflake_account_role" "data_engineer" {
   name = "DATA_ENGINEER"
 }
 
-# Data Analyst Role
 resource "snowflake_account_role" "data_analyst" {
   name = "DATA_ANALYST"
 }
 
-############################################
-# ROLE HIERARCHY
-############################################
-
-# DATA_ENGINEER inherits SYSADMIN_CUSTOM
 resource "snowflake_grant_account_role" "grant_data_engineer_to_sysadmin" {
   role_name        = snowflake_account_role.data_engineer.name
   parent_role_name = snowflake_account_role.sysadmin_custom.name
 }
 
-# DATA_ANALYST inherits SYSADMIN_CUSTOM
 resource "snowflake_grant_account_role" "grant_data_analyst_to_sysadmin" {
   role_name        = snowflake_account_role.data_analyst.name
   parent_role_name = snowflake_account_role.sysadmin_custom.name
 }
 
-############################################
-# WAREHOUSE ACCESS
-############################################
-
-# Warehouse usage for Data Engineer
 resource "snowflake_grant_privileges_to_account_role" "de_wh_usage" {
   privileges        = ["USAGE"]
   account_role_name = snowflake_account_role.data_engineer.name
-
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = "COMPUTE_WH"
   }
 }
 
-# Warehouse usage for Data Analyst
 resource "snowflake_grant_privileges_to_account_role" "da_wh_usage" {
   privileges        = ["USAGE"]
   account_role_name = snowflake_account_role.data_analyst.name
-
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = "COMPUTE_WH"
   }
 }
 
-############################################
-# DATABASE ACCESS
-############################################
-
-# Database usage for both roles
 resource "snowflake_grant_privileges_to_account_role" "db_usage_de" {
   privileges        = ["USAGE"]
   account_role_name = snowflake_account_role.data_engineer.name
@@ -84,11 +58,6 @@ resource "snowflake_grant_privileges_to_account_role" "db_usage_da" {
   }
 }
 
-############################################
-# SCHEMA ACCESS
-############################################
-
-# Schema usage for both roles
 resource "snowflake_grant_privileges_to_account_role" "schema_usage_de" {
   privileges        = ["USAGE"]
   account_role_name = snowflake_account_role.data_engineer.name
@@ -107,11 +76,6 @@ resource "snowflake_grant_privileges_to_account_role" "schema_usage_da" {
   }
 }
 
-############################################
-# TABLE PERMISSIONS
-############################################
-
-# Data Engineer - full access
 resource "snowflake_grant_privileges_to_account_role" "employee_full_access_de" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
   account_role_name = snowflake_account_role.data_engineer.name
@@ -122,7 +86,6 @@ resource "snowflake_grant_privileges_to_account_role" "employee_full_access_de" 
   }
 }
 
-# Data Analyst - read only
 resource "snowflake_grant_privileges_to_account_role" "employee_select_da" {
   privileges        = ["SELECT"]
   account_role_name = snowflake_account_role.data_analyst.name
@@ -133,11 +96,6 @@ resource "snowflake_grant_privileges_to_account_role" "employee_select_da" {
   }
 }
 
-############################################
-# USERS
-############################################
-
-# Create sample Data Engineer user
 resource "snowflake_user" "de_user" {
   name         = "DE_USER"
   login_name   = "DE_USER"
@@ -145,17 +103,12 @@ resource "snowflake_user" "de_user" {
   default_role = snowflake_account_role.data_engineer.name
 }
 
-# Create sample Data Analyst user
 resource "snowflake_user" "da_user" {
   name         = "DA_USER"
   login_name   = "DA_USER"
   password     = "TempPass@12345"
   default_role = snowflake_account_role.data_analyst.name
 }
-
-############################################
-# ASSIGN ROLES TO USERS
-############################################
 
 resource "snowflake_grant_account_role" "grant_de_role_to_user" {
   role_name = snowflake_account_role.data_engineer.name
